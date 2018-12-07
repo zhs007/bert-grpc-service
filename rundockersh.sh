4a9345a8845bbc53b265f28c7bcfa6311b85997b
zhs007 <sssxueren@gmail.com>
2018-12-07 15:58:07 +0800

    upd

--- rundockersh.sh
+++ rundockersh.sh
@@ -1,6 +1,9 @@
+docker stop bert-grpc-service
+docker rm bert-grpc-service
+
 docker run -it -d \
   --name bert-grpc-service \
   -v $PWD/modal:/bert-grpc-service \
   bert-grpc-service sh
-  
+
 docker exec -it bert-grpc-service sh
