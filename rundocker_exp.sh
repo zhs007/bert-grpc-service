dbc13d385ae6c62235de3c553868e5ad14c88466
zhs007 <sssxueren@gmail.com>
2018-12-16 20:35:41 +0800

    upd

--- rundocker_exp.sh
+++ rundocker_exp.sh
@@ -1,4 +1,4 @@
-docker run --runtime nvidia -it -rm \
+docker run --runtime nvidia -it --rm \
   -v $PWD/modal/cased_L-12_H-768_A-12:/bert-grpc-service/modal/ \
   -v $PWD/io:/bert-grpc-service/io \
   bert-grpc-service
