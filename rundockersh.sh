dd9e33c52157cd623d23b4c0483bb565b14d3b1a
zhs007 <sssxueren@gmail.com>
2018-12-16 16:38:26 +0800

    upd

--- rundockersh.sh
+++ rundockersh.sh
@@ -1,10 +1,10 @@
 docker stop bert-grpc-service
 docker rm bert-grpc-service
 
-docker run --runtime nvidia -it -d \
+docker run --runtime nvidia -it \
   --name bert-grpc-service \
   -v $PWD/modal/cased_L-12_H-768_A-12:/bert-grpc-service/modal/ \
   -v $PWD/io:/bert-grpc-service/io \
-  bert-grpc-service sh
+  bert-grpc-service
 
-docker exec -it bert-grpc-service sh
+# docker exec -it bert-grpc-service sh
